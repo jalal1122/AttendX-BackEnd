f1e4e12b43d32870e8e6128ef936e3ba6c1b3d86
muhammad uzair <abuzada735@gmail.com>
2025-09-02 16:46:44 +0500

    Refactor user model and environment configuration; add JWT authentication methods and nodemon for development

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,72 @@
-import  mongoose from "mongoose"
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// User schema definition
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  rollNo: {
+    type: Number,
+    required: true,
+  },
+  deviceIds: {
+    type: [String],
+    default: [],
+  },
+  refreshToken: {
+    type: String,
+    default: null,
+  },
+});
+
+// hash the password before saving the user
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  // generate salt
+  const salt = await bcryptjs.genSalt(10);
+
+  // generate password Hash
+  this.password = await bcryptjs.hash(this.password, salt);
+
+  // move to next function
+  next();
+});
+
+// Method to generate Access token
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ id: this._id, email: this.email }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+  });
+  return token;
+};
+
+// Method to generate Refresh Token
+userSchema.methods.generateRefreshToken = function () {
+  const token = jwt.sign({ id: this._id, email: this.email }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+  return token;
+};
+
+// Method to verify the password
+userSchema.methods.verifyPassword = async function (password) {
+  return await bcryptjs.compare(password, this.password);
+};
+
+// Adding the User Schema to the model
+const User = mongoose.model("User", userSchema);
+
+export default User;
