acb863af6df53dcf9739c05e56c09e626d2c8db5
muhammad uzair <abuzada735@gmail.com>
2025-09-03 00:00:22 +0500

    Add Cloudinary integration for file uploads; update environment variables and remove unused dependencies

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import ApiError from "../errors/ApiError.js";
+import asyncHandler from "./asyncHandler";
+
+// upload the file to cloudinary
+export const uploadOnCloudinary = asyncHandler(async (filePath) => {
+  // check the file path
+  if (!filePath) {
+    throw new ApiError(400, "No file uploaded");
+  }
+
+  // cloudinary configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+  // upload the file to cloudinary
+  const result = await cloudinary.uploader.upload(filePath, {
+    resource_type: "auto",
+    folder: "StallionWear",
+    quality: "auto",
+    fetch_format: "auto",
+  });
+
+  //   check the result
+  if (!result || !result.secure_url) {
+    throw new ApiError(500, "Error uploading file to Cloudinary");
+  }
+
+  return result.secure_url;
+});
