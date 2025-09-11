a6aa2b52bacb9125f4901bb1e6de25b7e2a9cf1c
Anis Riaz <masterkha123789@gmail.com>
2025-09-12 02:05:17 +0500

    Routes and Middleware

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,19 +1,24 @@
 import express from "express";
 import {
-    registerUser,
-    loginUser,
-    logoutUser,
-    refreshAccessToken
-} from "../controllers/user.controller.js"
-import authMiddleware from "../middleware/auth.middleware.js"
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
+import {
+  validateUserRegistration,
+  validateUserLogin,
+  handleValidationErrors,
+} from "../middleware/validate.middleware.js";
 
 const userRouter = express.Router();
 
 // User registration
-userRouter.post("/register", registerUser);
+userRouter.post("/register", validateUserRegistration, handleValidationErrors, registerUser);
 
 // User login
-userRouter.post("/login", loginUser);
+userRouter.post("/login", validateUserLogin, handleValidationErrors, loginUser);
 
 // User logout
 userRouter.post("/logout", authMiddleware, logoutUser);
@@ -22,4 +27,3 @@ userRouter.post("/logout", authMiddleware, logoutUser);
 userRouter.post("/refresh-token", refreshAccessToken);
 
 export default userRouter;
- 
